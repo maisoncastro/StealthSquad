17b85bd64ca0cf79caa6c0ef7f06cc1bd30dd7e4
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-04-13 19:39:41 -0400

    Added slider fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,93 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [includeUppercase, setIncludeUppercase] = useState(false);
+  const [includeLowercase, setIncludeLowercase] = useState(false);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
+  const [password, setPassword] = useState("");
+  const [length, setLength] = useState(12);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const generatePassword = () => {
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*()_-+=<>?/|{}[]~";
+
+    let allowedChars = "";
+    let generatedPassword = "";
+
+    if (includeUppercase) allowedChars += uppercase;
+    if (includeLowercase) allowedChars += lowercase;
+    if (includeNumbers) allowedChars += numbers;
+    if (includeSymbols) allowedChars += symbols;
+
+    if (!allowedChars) {
+      setErrorMessage("Please select at least one option.");
+      setPassword("");
+      return;
+    }
+
+    setErrorMessage("");
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * allowedChars.length);
+      generatedPassword += allowedChars[randomIndex];
+    }
+
+    setPassword(generatedPassword);
+  };
+
+  return (
+    <div className="App">
+      <h1>Password Generator</h1>
+      <label>
+        <input
+          type="checkbox"
+          checked={includeUppercase}
+          onChange={() => setIncludeUppercase(!includeUppercase)}
+        />
+        Include uppercase
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          checked={includeLowercase}
+          onChange={() => setIncludeLowercase(!includeLowercase)}
+        />
+        Include lowercase
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          checked={includeNumbers}
+          onChange={() => setIncludeNumbers(!includeNumbers)}
+        />
+        Include numbers
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          checked={includeSymbols}
+          onChange={() => setIncludeSymbols(!includeSymbols)}
+        />
+        Include symbols
+      </label>
+      <div>
+        <label>Password length: {length}</label>
+        <input
+          type="range"
+          min="5"
+          max="32"
+          value={length}
+          onChange={(e) => setLength(parseInt(e.target.value))}
+        />
+      </div>
+      <button onClick={generatePassword}>Click to Generate</button>
+      <p>{password}</p>
+    </div>
+  );
 }
 
 export default App;
